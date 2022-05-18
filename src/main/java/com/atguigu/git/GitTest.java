b21c4febf6dd21238e5542b285a12e701be8e74c
cw <1057477211@qq.com>
2022-05-18 15:09:30 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author cw
+ * @create 2022-05-18-15:05
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello.git!");
+    }
+}
